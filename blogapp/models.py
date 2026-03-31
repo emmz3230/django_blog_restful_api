5f9afabea2a578de74f701114a197893ae4744a6
emmz3230 <emmz3230@gmail.com>
2026-03-31 13:23:40 +0100

    Add Blog model and update CustomUser model in models.py; update compiled Python files

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-    
+from django.conf import settings   
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -25,3 +25,27 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Blog(models.Model):
+    CATEGORY = (
+        ('Technology', 'Technology'),
+        ("Economy", "Economy"),
+        ("Business", "Business"),
+        ("Sports", "Sports"),
+        ('Lifestyle', 'Lifestyle'),
+    )
+
+    title=models.CharField(max_length=255)
+    slug= models.SlugField(max_length=255,unique=True,blank=True)
+    content= models.TextField()
+    author= models.ForeignKey()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+    published_time= models.DateTimeField(blank=True,null=True)
+    is_draft= models.BooleanField(default=True)
+    category = models.CharField(max_length=255, choices=CATEGORY, blank=True, null=True)
+    featured_image = models.ImageField(upload_to='blog_img', blank=True, null=True)
+
+class Meta:
+    ordering = ['-created_at']
