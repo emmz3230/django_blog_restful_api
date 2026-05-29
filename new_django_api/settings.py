11143f6bd510a0c6d1ea5160e0db34c99aee7893
emmz3230 <emmz3230@gmail.com>
2026-05-29 02:25:00 +0100

    feat: initialize Django project settings with REST framework, JWT authentication, and CORS configuration

--- new_django_api/settings.py
+++ new_django_api/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-17y^zl_91(4dy%h-g8(yx)h)ro*j6^df+!spflxx99xg=g@q-q'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
